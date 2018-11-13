1d596fb2f97bba61632cd938aa5079e9155c2b0d
raitehu <pleiades-union@dune.ocn.ne.jp>
2018-11-14 07:07:14 +0900

    career system completed

--- app/controllers/careers_controller.rb
+++ app/controllers/careers_controller.rb
@@ -1,4 +1,5 @@
 class CareersController < ApplicationController
+  before_action :set_career, only: [:edit, :destroy] 
   def new
     @career = Career.new
   end
@@ -13,13 +14,28 @@ class CareersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @career = Career.find(params[:career][:id])
+    if @career.update(career_params)
+      redirect_to manage_url
+    else
+      render :edit
+    end    
+  end
+
   def destroy
-    Career.destroy(params[:id])
+    @career.destroy
     redirect_to manage_url
   end
 
   private
     def career_params
-      params.require(:career).permit(:the_day, :title, :description)
+      params.require(:career).permit(:the_day, :title, :description, :id)
+    end
+    def set_career
+      @career = Career.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   get 'manage', to: 'static_pages#manage'
 
-  get 'careers/new'
-  get 'careers/create'
-  get 'careers/destroy'
-  post 'career', to: 'careers#create'
-  delete 'career', to: 'careers#destroy'
-  patch 'career', to: 'careers#update'
+  resources :careers
+  post 'car', to: 'careers#create'
+  delete 'car', to: 'careers#destroy'
+  patch 'car', to: 'careers#update'
 
   resources :introductions
   post 'intro', to: 'introductions#create'
